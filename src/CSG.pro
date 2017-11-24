4e2cafdc6ce0bb85ccaf7389d3928372b5c181f6
Ricardo Ventrilho <ricardoventrilho@gmail.com>
2017-11-24 17:45:38 -0200

    Adicionando enumeradores

---
+++ src/CSG.pro
@@ -0,0 +1,10 @@
+TEMPLATE = app
+CONFIG += console c++14
+CONFIG -= app_bundle
+CONFIG -= qt
+LIBS += -lGL -lGLU -lglut
+
+SOURCES += main.cpp
+
+HEADERS += \
+    enumeradores/enumcsg.h
---
+++ src/enumeradores/enumcsg.h
@@ -0,0 +1,17 @@
+#ifndef ENUMCSG_H
+#define ENUMCSG_H
+
+namespace enumeradores
+{
+    enum EnumCSG
+    {
+        CSG_A,
+        CSG_B,
+        CSG_A_OR_B,
+        CSG_A_AND_B,
+        CSG_A_SUB_B,
+        CSG_B_SUB_A
+    };
+}
+
+#endif
